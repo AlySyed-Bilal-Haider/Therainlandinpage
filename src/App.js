d5d51c2130b1eee52e1a2ceb9c5f0bf4d6dbee38
AlySyed-Bilal-Haider <bilalshahbscs@gmail.com>
2022-05-18 04:41:27 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./Container/Header/Navbar.jsx";
+import Homepage from "./Container/Home/Homepage";
+import "./App.css";
+function App() {
+  return <>
+
+      <Navbar />
+      <Homepage />
+      </>
+}
+
+export default App;
